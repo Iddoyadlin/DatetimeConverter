f16a54ee81c21a10f37264f0b4147ca6d0f80f9c
ZviranOri <zviran.ori@gamil.com>
2018-11-05 19:52:42 +0200

    install java only if not installed yet add original java code and compile with the bash run

--- tests/java/install_and_run.sh
+++ tests/java/install_and_run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install oracle-java8-set-default
-echo -e "\n done installing... Gateway is up! \n"
-java -cp py4j0.10.8.1.jar:. java_gateway_server
+
+if ! [ -n `which java` ];
+then
+    echo -e "\n Installing Java! \n"
+    apt-get update && apt-get install oracle-java8-set-default
+else
+    echo -e "\n Compiling java gateway... \n"
+    javac  -cp py4j0.10.8.1.jar:. java_gateway_server.java
+    echo -e "\n Running the gateway :) \n"
+    java -cp py4j0.10.8.1.jar:. java_gateway_server
+fi
---
+++ tests/java/java_gateway_server.java
@@ -0,0 +1,8 @@
+import py4j.GatewayServer;
+
+public class java_gateway_server {
+  public static void main(String[] args) {
+    GatewayServer server = new GatewayServer();
+    server.start();
+  }
+}
