08001c38364bf60a4c5ab4dfba3244bb9a9c139d
ZviranOri <zviran.ori@gamil.com>
2018-11-07 19:40:01 +0200

    basic test with java server

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,5 +1,7 @@
 import unittest
-import os
+from tests.java.java_server import JavaServer
+
+from py4j.java_gateway import JavaGateway
 
 from patternconverter import PatternConverter
 
@@ -7,14 +9,27 @@ from patternconverter import PatternConverter
 class ConverterTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.java_gateway = JavaServer()
+        cls.java_gateway.run_server()
 
     @classmethod
     def tearDownClass(cls):
-        pass
+        cls.java_gateway.shutdown_server()
 
     def setUp(self):
-        pass
+        self.gateway = JavaGateway()
+
+    def test_year_month_day_format_conversion_to_java(self):
+        date = "2018-11-07"
+
+        python_format = "%Y-%M-%d"
+        c = PatternConverter()
+        converted_format = c.convert_to_iso_8601(pattern=python_format)
+
+        dt_formatter = self.gateway.jvm.java.time.format.DateTimeFormatter
+        formatter = dt_formatter.ofPattern(converted_format)
+
+        self.assertEqual(date.format(formatter), date)
 
     def test_python_to_iso_8601(self):
         python_format = '%Y%M%d'
