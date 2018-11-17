2e88dc456b170ecce4266f7ac426eac3dcf3e1d9
iddo <iddo.yadlin@neural-algorithms.com>
2018-11-17 18:54:05 +0200

    rename to directories

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Iddoyadlin/PythonToJavaDatetime",
-    packages=['datetime_converter'],
+    packages=['py2jdatetime'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
